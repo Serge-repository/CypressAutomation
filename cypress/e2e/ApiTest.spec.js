28c1dd01eabdd853b3c1342a19b29b7572fd0748
Serge-lab <sderii802@gmail.com>
2022-10-21 15:58:00 +0300

    tests added

--- cypress/e2e/ApiTest.spec.js
+++ cypress/e2e/ApiTest.spec.js
@@ -23,12 +23,28 @@ describe('Api test suite', () => {
         })
     })
 
-    it('Check response body continue', () => {
+    it('Check response body continue + headers', () => {
         cy.request({
             method: "GET",
             url: Cypress.env("apiBaseUrl2") + "/api/users"
         }).then((response) => {
             expect(response.body.data[0].id).to.eq(1);
+            expect(response.headers).to.have.property("server", "cloudflare");
+        })
+    })
+
+    it('POST request', () => {
+        cy.request({
+            method: "POST",
+            url: Cypress.env("apiBaseUrl2") + "/api/users",
+            body: {
+                "name": "morpheus",
+                "job": "leader"
+            }
+        }).then((response) => {
+            // see response in chrome console
+            cy.log(JSON.stringify(response));
+            expect(response.body).has.property("name", "morpheus");
         })
     })
 })
